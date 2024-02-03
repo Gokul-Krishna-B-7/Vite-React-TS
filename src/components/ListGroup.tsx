c5941140da542ea99565590f1005be341a9bff56
Gokul Krishna B <97348670+Gokul-Krishna-B-7@users.noreply.github.com>
2024-02-03 23:39:49 +0530

    Add files via upload

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,38 @@
+import { Fragment, useState } from "react";
+// { items: [], heading: string}
+interface Props {
+  items: string[];
+  heading: string[];
+  // (items: string) => void
+  onSelectItem: (item: string) => void; // onClick
+}
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  
+
+  // HOOK - A hook is an inbuilt react function that allows us to tap into built-in features of React.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            key={item}
+            onClick={() => {
+               setSelectedIndex(index);
+               onSelectItem(item);
+              }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
